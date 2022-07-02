572cdace6c71ebb51eeb50b04f359315e79003eb
FLJ <617229004@qq.com>
2022-07-02 18:12:39 +0800

    react router study

--- src/index.js
+++ src/index.js
@@ -3,11 +3,42 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Expreses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element = {<App />} >
+                  <Route path="expenses" element = {<Expreses />} />
+                  <Route path="invoices" element = {<Invoices />} >
+                      {/*默认路由,也可以说叫做 index 路由*/}
+                      {/*<Route index element = {*/}
+                      {/*    <main style={{padding: "1rem"}}>*/}
+                      {/*        <p>Select an invoice</p>*/}
+                      {/*    </main>*/}
+                      {/*}/>*/}
+
+                      {/*默认路由*/}
+                      <Route path="" element = {
+                          <main style={{padding: "1rem"}}>
+                              <p>请选择一个invoice</p>
+                          </main>
+                      }/>
+                      <Route path=":invoiceId" element = {<Invoice />} />
+                  </Route>
+                  <Route path="*" element = {
+                      <main style={{padding: "1rem"}}>
+                          <p>There's nothing here !!!</p>
+                      </main>
+                  } />
+              </Route>
+          </Routes>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
